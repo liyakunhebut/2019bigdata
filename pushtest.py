c619114c1cd723e5dae71f5bc445f6dceadd9f8e
liyakunhebut <liyak@126.com>
2019-11-03 19:49:21 +0800

    mytest

--- pushtest.py
+++ pushtest.py
@@ -1,17 +1,6 @@
-print("hello world")
+print("hello worl")
 a = 0
 b = 1
 c = 2
 d,e,f = 3,4,5
 print(a,b,c,d,e,f)
-
-num = 1
-if num == 1 or num == 3 or num == 5:
-    type = '奇数'
-print(type)
-
-num = 1
-if num in(1,3,5):
-    type = '奇数'
-print(type)
-
